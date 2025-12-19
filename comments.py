b059a78c317c49fd4abeee049b1435b2f9ead937
sarismejiasanchez <sarismejiasanchez@gmail.com>
2025-12-19 16:57:01 -0500

    6. Comments in Python: single-line and multi-line

---
+++ comments.py
@@ -0,0 +1,11 @@
+# This is a comment line in Python
+print ("Hello, World!")  # This prints a greeting message
+
+# This
+# is a multi-line comment
+# explaining the following code block
+
+"""
+This is a multi-line comments
+using triple quotes.
+"""
