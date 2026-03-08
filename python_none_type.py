8bc4e017b52f6d96d2e9a26030e439b4ca1c97a2
sarismejiasanchez <sarismejiasanchez@gmail.com>
2026-03-08 15:11:39 -0500

    14. The None data type in Python

---
+++ python_none_type.py
@@ -0,0 +1,13 @@
+# None is a special data type in Python that represents the absence of a value or a null value. 
+# It is often used to indicate that a variable has no value or that a function does not return anything.
+
+x = None
+print(x) # Output: None
+print(type(x)) # Output: <class 'NoneType'>
+
+print(type("")) # Output: <class 'str'>
+print(type(0)) # Output: <class 'int'>
+print(type([])) # Output: <class 'list'>
+print(type({})) # Output: <class 'dict'>
+print(type(())) # Output: <class 'tuple'>
+print(type(False)) # Output: <class 'bool'>
